9b5a9794bd62ed34b101309c3576e63f3f2602ad
Amanmehta666 <110601746+Amanmehta666@users.noreply.github.com>
2026-02-21 01:18:55 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggle = document.getElementById("menu-toggle");
-const nav = document.getElementById("nav");
-
-toggle.addEventListener("click", () => {
-    nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-});
